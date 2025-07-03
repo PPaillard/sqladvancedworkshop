68bd7dd669c4eed38892579a1aecf54d7577b2fc
PierreP <53476143+RoYaLSleT@users.noreply.github.com>
2025-07-03 14:47:51 +0200

    modification moyenne age

--- workshop.sql
+++ workshop.sql
@@ -1,46 +1,45 @@
 USE kaamelott;
 
--- Dans un fichier .sql, les tirets en début de ligne permettent de commenter du code 
--- afin qu'il ne soit pas exécuté
--- ⬇ écrire les requêtes de l'atelier ici ⬇
+-- Dans un fichier .sql, les tirets en debut de ligne permettent de commenter du code 
+-- afin qu'il ne soit pas execute
+-- ⬇ ecrire les requêtes de l'atelier ici ⬇
+-- Le prenom, nom et age des personnages
+SELECT firstname, lastname, age FROM person;
 
--- Le prénom, nom et âge des personnages
-SELECT firstname,lastname, age FROM person;
+-- Le prenom, nom des personnages ainsi que leur royaume, uniquement pour ceux etant relies à un royaume
+SELECT firstname, lastname, name AS kingdom_Name FROM person 
+JOIN kingdom ON person.kingdom_id = kingdom.id;
 
--- Le prénom, nom des personnages ainsi que leur royaume, uniquement pour ceux étant reliés à un royaume
-SELECT p.firstname,p.lastname, k.name FROM person p 
-JOIN kingdom k ON p.kingdom_id = k.id;
+-- Le prenom, nom des personnages ainsi que leur royaume, qu'ils aient un royaume ou non.
+SELECT firstname, lastname, name AS kingdom_name FROM person 
+LEFT JOIN kingdom ON kingdom.id = person.kingdom_id;
 
--- La même chose en incluant tous les personnages
-SELECT p.firstname,p.lastname, k.name FROM person p 
-LEFT JOIN kingdom k ON p.kingdom_id = k.id;
+-- La moyenne de l’age des personnages
+SELECT AVG(age) AS avg_age FROM PERSON;
 
--- La moyenne de l’âge des personnages
-SELECT AVG(age) FROM person;
-
--- La moyenne de tous les personnages n’ayant pas le rôle de magicien
-SELECT AVG(p.age) FROM person p 
-JOIN role r ON p.role_id = r.id
-WHERE r.role != 'magicien';
+-- La moyenne d'age de tous les personnages n’ayant pas le rôle de magicien
+SELECT AVG(age) AS avg_age FROM person
+LEFT JOIN role ON role.id = person.role_id
+WHERE person.role_id IS NULL OR role.role != "magicien";
 
 -- Le nombre de personnage par royaume (inclure les royaumes n’ayant pas de personnage)
-SELECT COUNT(*) as nb, k.name FROM person p
-RIGHT JOIN kingdom k ON p.kingdom_id = k.id
-GROUP BY k.name;
+SELECT name, COUNT(person.id) as nb_person_Kingdom FROM kingdom
+LEFT JOIN person ON kingdom.id = person.kingdom_id
+GROUP BY kingdom.name;
 
--- La moyenne de l’âge par rôle
-SELECT AVG(p.age) as nb, r.role FROM person p
-JOIN role r ON p.role_id = r.id
-GROUP BY r.role;
+-- La moyenne de l’age par rôle
+SELECT role, AVG(age) as avg_age FROM person
+LEFT JOIN role ON role.id = person.role_id
+GROUP BY role;
 
--- La liste de tous les personnages avec leur rôle et royaume éventuels
-SELECT CONCAT(p.firstname, ' ', IFNULL(p.lastname,'')) as fullname, r.role, k.name FROM person p
-LEFT JOIN kingdom k ON p.kingdom_id = k.id
-LEFT JOIN role r ON p.role_id = r.id;
+
+-- La liste de tous les personnages avec leur rôle et royaume eventuels
+SELECT firstname, lastname, name AS royaume, role FROM person as p
+LEFT JOIN kingdom AS k ON k.id = p.kingdom_id
+LEFT JOIN role AS r ON r.id = p.role_id;
 
 -- La liste des royaumes ayant au moins 2 sujets
--- Le nombre de personnage par royaume (inclure les royaumes n’ayant pas de personnage)
-SELECT COUNT(*) as nb, k.name FROM person p
-RIGHT JOIN kingdom k ON p.kingdom_id = k.id
-GROUP BY k.name
-HAVING nb >= 2;
+SELECT name, COUNT(person.id) as nb_person_Kingdom FROM kingdom
+JOIN person ON kingdom.id = person.kingdom_id
+GROUP BY kingdom.name
+HAVING nb_person_Kingdom >= 2;
